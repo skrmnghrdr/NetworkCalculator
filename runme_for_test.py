44221edc19d9882ed2151b3f326d17d37b140b8b
skrmnghrdr <notpaulogrian@protonmail.com>
2024-09-25 14:57:31 -0400

    added reminders how to git via terminal

--- runme_for_test.py
+++ runme_for_test.py
@@ -3,11 +3,16 @@
 #hey, i'm using vim okay.
 
 
-
+"""
+git add (things)
+git status (to see status)
+git commit -m "define what milestone did you accomplish on this commit"
+git push origin main (push to git site)
+"""
 
 import Hostcalculator
 
 
 a = Hostcalculator.hosts(verbose=True).cidr_to_fullmask(2)
-
+b = Hostcalculator.hosts
 pass
